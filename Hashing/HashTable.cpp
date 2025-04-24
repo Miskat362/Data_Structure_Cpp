a623c43bdbd3afb6c90fceffe6ba8c6f80a6793e
Miskat362 <memiskat362@gmail.com>
2025-04-24 23:58:56 +0600

    24-04-2025

---
+++ Hashing/HashTable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Hash Table ia a unordered data structure (key, value)
+    // Insert O(1)
+    // Search O(1)
+    // Remove O(1)
+
+    // hash function -> index = hash(key) % size
+    // hash function -> index = (key % size) + 1
+
+    return 0;
+}
