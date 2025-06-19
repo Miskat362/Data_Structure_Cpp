de9ef2541e3791e164b456000686c953bd82f950
Miskat362 <memiskat362@gmail.com>
2025-06-19 20:07:27 +0600

    19-06-2025

---
+++ Graphs/CheapestFlightsWithinKStops.cpp
@@ -0,0 +1,45 @@
+// leetcode 787
+class Solution  {
+public:
+    class Info {
+        int u;
+        int cost;
+        int stops; // src to u
+
+        Info(int u, int cost, int stops) {
+            this->u = u;
+            this->cost = cost;
+            this->stops = stops;
+        }
+    };
+
+    int findCheapestPrices(int n, vector<vector<int>> &flights, int src, int dst, int k) {
+        queue<Info> q;
+        vector<int> dist(V, INT32_MAX);
+
+        dist[src] = 0;
+        q.push(Info(src, 0, -1));
+
+        while(q,size() > 0) {
+            Info curr = q.front();
+            q.pop();
+
+            for(int i=0; i<flights.size(); i++) {
+                if(flights[i][0] = curr.u) {
+                    int v = flights[i][1];
+                    int wt = flights[i][2];
+
+                    if(dist[v] > curr.cost + wt && curr.stops+1 <=k) {
+                        dist[v] = curr.cost + wt
+                        q.push(Info(v, dist[v], curr.stops+1));
+                    }
+                }
+            }
+        }
+        if(dist[dst] == INT32_MAX) {
+            return -1;
+        }
+        return dist[dst];
+    }
+
+};
---
+++ Graphs/CourseSchedule.cpp
@@ -0,0 +1,41 @@
+// Leetcode 207
+
+class Solution {
+public:
+    bool isCycle(int src, vector<bool> &vis, vector<bool> &rec, vector<vector<int>> &graph){
+        vis[src] = true;
+        rec[src] = true;
+
+        for(int i=0; i<graph.size(); i++){
+            int u = graph[i][1];
+            int v = graph[i][0];
+            if(u == src){
+                if(!vis[v]){
+                    if(isCycle(v, vis, rec, graph)){
+                        return true;
+                    }
+                } else {
+                    is(rec[v]){
+                        return true;
+                    }
+                }
+            }
+        }
+        rec[src] = false;
+        return false;
+    }
+    bool canFinish(int numCourses, vector<vector<int>> &graph) {
+        int V = numCourses;     // [v,u] v<--u
+        vector<bool> vis(V, false);
+        vector<bool> rec(V, false);
+
+        for(int i=0; i<V; i++){
+            if(!vis[i]){
+                if(isCycle(1, vis, rec, graph)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
